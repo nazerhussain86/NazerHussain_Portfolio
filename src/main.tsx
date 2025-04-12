895f468ea2c4734bcce833ec1c5578150ba571d7
Nazer Hussain Abdul Raheem <nazerhussain1999@gmail.com>
2025-04-13 00:14:02 +0530

    nazer

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById("root")!).render(
-   
-        <App />
-   
+        <HashRouter>
+                <App />
+        </HashRouter>
 );
