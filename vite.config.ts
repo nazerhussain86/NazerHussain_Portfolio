6d102f98dd6c472e7ec65d0b3ba6be35b6e7462f
Nazer Hussain Abdul Raheem <nazerhussain1999@gmail.com>
2025-04-12 23:01:07 +0530

    Nazer Hussain New changes error fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+   base: "/NazerHussain_Portfolio/", // 👈 Add this line
   server: {
     host: "::",
     port: 8080,
